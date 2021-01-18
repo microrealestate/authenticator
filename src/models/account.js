79425eb07677318fa31e21bccd8b66e94e1d0912
Camel Aissani <camel.aissani@nuageprive.fr>
2021-01-18 01:28:28 +0100

    updated with the new Realm model

--- src/models/account.js
+++ src/models/account.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const RealmModel = require('./realm');
 
 const AccountSchema = new mongoose.Schema({
     firstname: {
@@ -25,13 +26,33 @@ const AccountSchema = new mongoose.Schema({
     createdDate: Date
 });
 
+// lowercase email
 // hash user password before saving into database
 AccountSchema.pre('save', function (next) {
     if (!this.createdDate) {
         this.createdDate = new Date();
     }
+    this.email = this.email.toLowerCase();
     this.password = bcrypt.hashSync(this.password, 10);
     next();
+});
+
+AccountSchema.post('save', function (account) {
+    const name = `${account.firstname} ${account.lastname}`;
+    RealmModel.updateMany({
+        members: {
+            $elemMatch: {email: account.email}
+        }
+    }, {
+        $set: {
+            'members.$.registered': true,
+            'members.$.name': name
+        }
+    }, (error) => {
+        if (error) {
+            console.error(error);
+        }
+    });
 });
 
 module.exports = mongoose.model('Account', AccountSchema);
