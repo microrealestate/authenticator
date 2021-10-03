82e4fe62528834250686979a3cba31bba86bf4cb
Camel Aissani <camel.aissani@nuageprive.fr>
2021-10-03 19:58:29 +0200

    fixed issue https://github.com/microrealestate/microrealestate/issues/14

--- src/apirouter.js
+++ src/apirouter.js
@@ -9,55 +9,57 @@ const redisClient = require('./redisclient');
 const AccountModel = require('./models/account');
 
 const refreshTokenCookieAttributes = {
-    httpOnly: true,
-    sameSite: true,
-    secure: config.DOMAIN !== 'localhost',
-    domain: config.DOMAIN
+  httpOnly: true,
+  sameSite: true,
+  secure: config.DOMAIN !== 'localhost',
+  domain: config.DOMAIN,
 };
 
-const _generateTokens = async dbAccount => {
-    const { _id, password, ...account } = dbAccount;
-    const refreshToken = jwt.sign({ account }, config.REFRESH_TOKEN_SECRET);
-    const accessToken = jwt.sign({ account }, config.ACCESS_TOKEN_SECRET, { expiresIn: '5m' });
+const _generateTokens = async (dbAccount) => {
+  const { _id, password, ...account } = dbAccount;
+  const refreshToken = jwt.sign({ account }, config.REFRESH_TOKEN_SECRET);
+  const accessToken = jwt.sign({ account }, config.ACCESS_TOKEN_SECRET, {
+    expiresIn: '5m',
+  });
 
-    // save tokens
-    await redisClient.set(refreshToken, accessToken);
+  // save tokens
+  await redisClient.set(refreshToken, accessToken);
 
-    return {
-        refreshToken,
-        accessToken
+  return {
+    refreshToken,
+    accessToken,
+  };
+};
+
+const _refreshTokens = async (oldRefreshToken) => {
+  const oldAccessToken = await redisClient.get(oldRefreshToken);
+  if (!oldAccessToken) {
+    logger.error('refresh token not found in database');
+    return {};
+  }
+
+  let account;
+  try {
+    jwt.verify(oldAccessToken, config.ACCESS_TOKEN_SECRET);
+    const payload = jwt.verify(oldRefreshToken, config.REFRESH_TOKEN_SECRET);
+    if (payload && payload.account) {
+      account = payload.account;
     }
-}
+  } catch (exc) {
+    logger.error(exc);
+  }
+  await _clearTokens(oldRefreshToken);
 
-const _refreshTokens = async oldRefreshToken => {
-    const oldAccessToken = await redisClient.get(oldRefreshToken);
-    if (!oldAccessToken) {
-        logger.error('refresh token not found in database');
-        return {};
-    }
+  if (!account) {
+    return {};
+  }
 
-    let account;
-    try {
-        jwt.verify(oldAccessToken, config.ACCESS_TOKEN_SECRET);
-        payload = jwt.verify(oldRefreshToken, config.REFRESH_TOKEN_SECRET);
-        if (payload && payload.account) {
-            account = payload.account;
-        }
-    } catch (exc) {
-        logger.error(exc);
-    }
-    await _clearTokens(oldRefreshToken);
+  return await _generateTokens(account);
+};
 
-    if (!account) {
-        return {};
-    }
-
-    return await _generateTokens(account);
-}
-
-const _clearTokens = async refreshToken => {
-    await redisClient.del(refreshToken);
-}
+const _clearTokens = async (refreshToken) => {
+  await redisClient.del(refreshToken);
+};
 
 const apiRouter = express.Router();
 
@@ -65,163 +67,178 @@ const apiRouter = express.Router();
 apiRouter.use(locale(['fr-FR', 'en-US', 'pt-BR'], 'en-US'));
 
 if (config.SIGNUP) {
-    apiRouter.post('/signup', async (req, res) => {
-        try {
-            const { firstname, lastname, email, password } = req.body;
-            if ([firstname, lastname, email, password].map(el => el.trim()).some(el => !!!el)) {
-                return res.status(422).json({ error: 'missing fields' });
-            }
-            const existingAccount = await AccountModel.findOne({ email: email.toLowerCase() });
-            if (existingAccount) {
-                return res.sendStatus(409);
-            }
-            await AccountModel.create({ firstname, lastname, email, password });
-            res.sendStatus(201);
-        } catch (exc) {
-            res.sendStatus(500);
-        }
-    });
+  apiRouter.post('/signup', async (req, res) => {
+    try {
+      const { firstname, lastname, email, password } = req.body;
+      if (
+        [firstname, lastname, email, password]
+          .map((el) => el.trim())
+          .some((el) => !!el === false)
+      ) {
+        return res.status(422).json({ error: 'missing fields' });
+      }
+      const existingAccount = await AccountModel.findOne({
+        email: email.toLowerCase(),
+      });
+      if (existingAccount) {
+        return res.sendStatus(409);
+      }
+      await AccountModel.create({ firstname, lastname, email, password });
+      res.sendStatus(201);
+    } catch (exc) {
+      res.sendStatus(500);
+    }
+  });
 }
 
 apiRouter.post('/signin', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        if ([email, password].map(el => el.trim()).some(el => !!!el)) {
-            logger.info('login failed some fields are missing');
-            return res.status(422).json({ error: 'missing fields' });
-        }
-
-        const account = await AccountModel.findOne({ email: email.toLowerCase() });
-        if (!account) {
-            logger.info(`login failed for ${email} account not found`);
-            return res.sendStatus(401);
-        }
-
-        const validPassword = await bcrypt.compare(password, account.password);
-        if (!validPassword) {
-            logger.info(`login failed for ${email} bad password`);
-            return res.sendStatus(401);
-        }
-
-        const { refreshToken, accessToken } = await _generateTokens(account.toObject());
-
-        logger.debug(`create a new refresh token ${refreshToken} for domain ${req.hostname}`);
-        res.cookie('refreshToken', refreshToken, refreshTokenCookieAttributes);
-        res.json({
-            accessToken
-        });
-    } catch (exc) {
-        logger.error(exc);
-        res.sendStatus(500);
+  try {
+    const { email, password } = req.body;
+    if ([email, password].map((el) => el.trim()).some((el) => !!el === false)) {
+      logger.info('login failed some fields are missing');
+      return res.status(422).json({ error: 'missing fields' });
     }
+
+    const account = await AccountModel.findOne({ email: email.toLowerCase() });
+    if (!account) {
+      logger.info(`login failed for ${email} account not found`);
+      return res.sendStatus(401);
+    }
+
+    const validPassword = await bcrypt.compare(password, account.password);
+    if (!validPassword) {
+      logger.info(`login failed for ${email} bad password`);
+      return res.sendStatus(401);
+    }
+
+    const { refreshToken, accessToken } = await _generateTokens(
+      account.toObject()
+    );
+
+    logger.debug(
+      `create a new refresh token ${refreshToken} for domain ${req.hostname}`
+    );
+    res.cookie('refreshToken', refreshToken, refreshTokenCookieAttributes);
+    res.json({
+      accessToken,
+    });
+  } catch (exc) {
+    logger.error(exc);
+    res.sendStatus(500);
+  }
 });
 
 apiRouter.post('/refreshtoken', async (req, res) => {
-    const oldRefreshToken = req.cookies.refreshToken;
-    logger.debug(`give a new refresh token for ${oldRefreshToken}`);
-    if (!oldRefreshToken) {
-        return res.sendStatus(401);
+  const oldRefreshToken = req.cookies.refreshToken;
+  logger.debug(`give a new refresh token for ${oldRefreshToken}`);
+  if (!oldRefreshToken) {
+    return res.sendStatus(401);
+  }
+
+  try {
+    const { refreshToken, accessToken } = await _refreshTokens(oldRefreshToken);
+    if (!refreshToken) {
+      return res.sendStatus(403);
     }
 
-    try {
-        const { refreshToken, accessToken } = await _refreshTokens(oldRefreshToken);
-        if (!refreshToken) {
-            return res.sendStatus(403);
-        }
-
-        res.cookie('refreshToken', refreshToken, refreshTokenCookieAttributes);
-        res.json({
-            accessToken
-        });
-    } catch (exc) {
-        logger.error(exc);
-        res.sendStatus(500);
-    }
-
+    res.cookie('refreshToken', refreshToken, refreshTokenCookieAttributes);
+    res.json({
+      accessToken,
+    });
+  } catch (exc) {
+    logger.error(exc);
+    res.sendStatus(500);
+  }
 });
 
 apiRouter.delete('/signout', async (req, res) => {
-    const refreshToken = req.cookies.refreshToken;
-    logger.debug(`remove the refresh token: ${refreshToken}`);
-    if (!refreshToken) {
-        return res.sendStatus(202);
-    }
+  const refreshToken = req.cookies.refreshToken;
+  logger.debug(`remove the refresh token: ${refreshToken}`);
+  if (!refreshToken) {
+    return res.sendStatus(202);
+  }
 
-    try {
-        res.clearCookie('refreshToken', refreshTokenCookieAttributes);
-        await _clearTokens(refreshToken);
-        res.sendStatus(204);
-    } catch (exc) {
-        logger.error(exc);
-        res.sendStatus(500);
-    }
+  try {
+    res.clearCookie('refreshToken', refreshTokenCookieAttributes);
+    await _clearTokens(refreshToken);
+    res.sendStatus(204);
+  } catch (exc) {
+    logger.error(exc);
+    res.sendStatus(500);
+  }
 });
 
 apiRouter.post('/forgotpassword', async (req, res) => {
-    try {
-        const { email } = req.body;
-        if (!email) {
-            return res.status(422).json({ error: 'missing fields' });
-        }
-        // check if user exists
-        const account = await AccountModel.findOne({ email: email.toLowerCase() });
-        if (account) {
-            // generate reset token valid for one hour
-            const token = jwt.sign({ email }, config.RESET_TOKEN_SECRET, { expiresIn: '1h' });
-            await redisClient.set(token, email);
-
-            // send email
-            await axios.post(config.EMAILER_URL, {
-                templateName: 'reset_password',
-                recordId: email,
-                params: {
-                    token
-                }
-            }, {
-                headers: {
-                    'Accept-Language': req.rawLocale.code
-                }
-            });
-        }
-        res.sendStatus(204);
-    } catch (error) {
-        logger.error(error.data ? error.data : error);
-        res.sendStatus(500);
+  try {
+    const { email } = req.body;
+    if (!email) {
+      return res.status(422).json({ error: 'missing fields' });
     }
+    // check if user exists
+    const account = await AccountModel.findOne({ email: email.toLowerCase() });
+    if (account) {
+      // generate reset token valid for one hour
+      const token = jwt.sign({ email }, config.RESET_TOKEN_SECRET, {
+        expiresIn: '1h',
+      });
+      await redisClient.set(token, email);
+
+      // send email
+      await axios.post(
+        config.EMAILER_URL,
+        {
+          templateName: 'reset_password',
+          recordId: email,
+          params: {
+            token,
+          },
+        },
+        {
+          headers: {
+            'Accept-Language': req.rawLocale.code,
+          },
+        }
+      );
+    }
+    res.sendStatus(204);
+  } catch (error) {
+    logger.error(error.data ? error.data : error);
+    res.sendStatus(500);
+  }
 });
 
 apiRouter.patch('/resetpassword', async (req, res) => {
-    const { resetToken, password } = req.body;
-    if ([resetToken, password].map(el => el.trim()).some(el => !!!el)) {
-        return res.status(422).json({ error: 'missing fields' });
-    }
+  const { resetToken, password } = req.body;
+  if (
+    [resetToken, password].map((el) => el.trim()).some((el) => !!el === false)
+  ) {
+    return res.status(422).json({ error: 'missing fields' });
+  }
 
-    const email = await redisClient.get(resetToken);
-    if (!email) {
-        return res.sendStatus(403);
-    }
+  const email = await redisClient.get(resetToken);
+  if (!email) {
+    return res.sendStatus(403);
+  }
 
-    await redisClient.del(resetToken);
+  await redisClient.del(resetToken);
 
-    try {
-        jwt.verify(resetToken, config.RESET_TOKEN_SECRET);
-    } catch (error) {
-        console.error(error);
-        return res.sendStatus(403);
-    }
+  try {
+    jwt.verify(resetToken, config.RESET_TOKEN_SECRET);
+  } catch (error) {
+    console.error(error);
+    return res.sendStatus(403);
+  }
 
-    try {
-        const account = await AccountModel.findOne(
-            { email: email.toLowerCase() }
-        );
-        account.password = password;
-        await account.save();
-    } catch (error) {
-        logger.error(error);
-        res.sendStatus(500);
-    }
+  try {
+    const account = await AccountModel.findOne({ email: email.toLowerCase() });
+    account.password = password;
+    await account.save();
+  } catch (error) {
+    logger.error(error);
+    res.sendStatus(500);
+  }
 
-    res.sendStatus(200);
+  res.sendStatus(200);
 });
 
 module.exports = apiRouter;
